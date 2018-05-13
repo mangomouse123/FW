0d56123144dc848885807a9ca378aa7e39c5d929
atmutex <mutexsharma@gmail.com>
2018-05-13 09:08:12 +0530

    CTS_WORKING_SAMPLE

---
+++ CTS_/pca10028/s130/armgcc/_build/nrf51422_xxac_hardfault_implementation.c.d
@@ -0,0 +1,122 @@
+_build/nrf51422_xxac_hardfault_implementation.c.o: \
+ ../../../../../../components/libraries/hardfault/hardfault_implementation.c \
+ ../../../../../../components/libraries/util/sdk_common.h \
+ e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\lib\gcc\arm-none-eabi\7.2.1\include\stdint.h \
+ e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\stdint.h \
+ e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\machine\_default_types.h \
+ e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\sys\features.h \
+ e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\_newlib_version.h \
+ e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\sys\_intsup.h \
+ e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\sys\_stdint.h \
+ e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\lib\gcc\arm-none-eabi\7.2.1\include\stdbool.h \
+ e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\string.h \
+ e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\_ansi.h \
+ e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\newlib.h \
+ e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\sys\config.h \
+ e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\machine\ieeefp.h \
+ e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\sys\reent.h \
+ e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\_ansi.h \
+ e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\lib\gcc\arm-none-eabi\7.2.1\include\stddef.h \
+ e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\sys\_types.h \
+ e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\machine\_types.h \
+ e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\sys\lock.h \
+ e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\sys\cdefs.h \
+ e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\sys\string.h \
+ ../config/sdk_config.h \
+ ../../../../../../components/libraries/util/nordic_common.h \
+ ../../../../../../components/device/compiler_abstraction.h \
+ ../../../../../../components/libraries/util/sdk_os.h \
+ ../../../../../../components/libraries/util/sdk_errors.h \
+ ../../../../../../components/softdevice/s130/headers/nrf_error.h \
+ ../../../../../../components/libraries/util/app_util.h \
+ ../../../../../../components/device/nrf.h \
+ ../../../../../../components/device/nrf51.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm0.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/system_nrf51.h \
+ ../../../../../../components/device/nrf51_bitfields.h \
+ ../../../../../../components/device/nrf51_deprecated.h \
+ ../../../../../../components/device/compiler_abstraction.h \
+ ../../../../../../components/libraries/util/sdk_macros.h
+
+../../../../../../components/libraries/util/sdk_common.h:
+
+e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\lib\gcc\arm-none-eabi\7.2.1\include\stdint.h:
+
+e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\stdint.h:
+
+e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\machine\_default_types.h:
+
+e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\sys\features.h:
+
+e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\_newlib_version.h:
+
+e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\sys\_intsup.h:
+
+e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\sys\_stdint.h:
+
+e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\lib\gcc\arm-none-eabi\7.2.1\include\stdbool.h:
+
+e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\string.h:
+
+e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\_ansi.h:
+
+e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\newlib.h:
+
+e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\sys\config.h:
+
+e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\machine\ieeefp.h:
+
+e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\sys\reent.h:
+
+e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\_ansi.h:
+
+e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\lib\gcc\arm-none-eabi\7.2.1\include\stddef.h:
+
+e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\sys\_types.h:
+
+e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\machine\_types.h:
+
+e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\sys\lock.h:
+
+e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\sys\cdefs.h:
+
+e:\personal\installed\gnu\ tools\ arm\ embedded\7\ 2017-q4-major\arm-none-eabi\include\sys\string.h:
+
+../config/sdk_config.h:
+
+../../../../../../components/libraries/util/nordic_common.h:
+
+../../../../../../components/device/compiler_abstraction.h:
+
+../../../../../../components/libraries/util/sdk_os.h:
+
+../../../../../../components/libraries/util/sdk_errors.h:
+
+../../../../../../components/softdevice/s130/headers/nrf_error.h:
+
+../../../../../../components/libraries/util/app_util.h:
+
+../../../../../../components/device/nrf.h:
+
+../../../../../../components/device/nrf51.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cm0.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmInstr.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmFunc.h:
+
+../../../../../../components/toolchain/system_nrf51.h:
+
+../../../../../../components/device/nrf51_bitfields.h:
+
+../../../../../../components/device/nrf51_deprecated.h:
+
+../../../../../../components/device/compiler_abstraction.h:
+
+../../../../../../components/libraries/util/sdk_macros.h:
---
+++ CTS_/readme.md
@@ -0,0 +1,5 @@
+This is working sample firware CTS service.
+Logs can be seen on COM port. 
+
+Make sure -- 
+macro #NRF_LOG_ENABLED is set to 1 to enable NRF logs
